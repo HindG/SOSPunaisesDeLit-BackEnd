8b47993356b128ee369fd4145a9c6de9e2192f1f
Hind Guessous <guessoushind@gmail.com>
2023-10-13 11:22:07 +0200

    :tada:

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const fs = require('fs');
+
+app.use(express.json());
+var cors = require('cors');
+app.use(cors({ origin: true, credentials: true }));
+const port = 3000
+
+const map_points = require('./mappoints.json')
+
+// API routes
+
+app.get('/map_points', (req, res) => {
+    res.status(200).json(map_points)
+})
+
+app.post('/map_points', (req, res) => {
+    map_points.push(req.body)
+    fs.writeFile(`./mappoints.json`, JSON.stringify(map_points, null, 2), (err) => { 
+        if (err) 
+          console.log(err); 
+        else { 
+          console.log("File written successfully\n"); 
+        } 
+      });
+    res.status(200).json(map_points)
+})
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+});
