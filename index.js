ecb378e2e587bd58837235a6bf1c21e86df8b5e0
Hind Guessous <guessoushind@gmail.com>
2023-10-13 12:27:32 +0200

    :bug:

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 app.use(express.json());
 var cors = require('cors');
 app.use(cors({ origin: true, credentials: true }));
-const port = 3000
+const port = process.env.PORT || 3000
 
 const map_points = require('./mappoints.json')
 
